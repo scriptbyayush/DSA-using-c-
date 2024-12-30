cbb2e8a1cad801731fdd5ba1e2cc49aa26bf517e
scriptbyayush <ayushvaidya41154115@gmail.com>
2024-12-30 15:16:41 +0530

    Initial Commit

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <type_traits>
+
 using namespace std;
 
-struct Node
-{
+struct Node{
     int data;
     Node *next;
 };
@@ -17,30 +17,31 @@ class LinkedList
     public:
     LinkedList() : head(nullptr){}
 
-    void insertAtBeginning(int value)
-    {
+    void insertAtBeginning(int value){
+
         Node *newNode=new Node();
         newNode->data=value;
         newNode->next=head;
         head=newNode;
+
         return;
     }
 
-    void insertAtEnd(int value)
-    {
-        Node *newNode=new Node();
+    void insertAtEnd(int value){
+
+        Node *newNode=new Node;
         newNode->data=value;
         newNode->next=nullptr;
 
         if(head==nullptr)
         {
+
             head=newNode;
-            return;
         }else{
 
             Node *temp=head;
-            while(temp->next!=nullptr)
-            {
+            while(temp->next!=nullptr){
+
                 temp=temp->next;
             }
             temp->next=newNode;
@@ -49,18 +50,20 @@ class LinkedList
         return;
     }
 
-    void insertAfter(int value,int targetValue)
-    {
-        Node *temp=head;
 
+    void insertAfter(int value,int targetValue){
+
+        Node *temp=head;
         while(temp!=nullptr&&temp->data!=targetValue)
         {
+
             temp=temp->next;
         }
 
         if(temp==nullptr)
         {
-            cout<<"not found : "<<targetValue<<endl<<endl;
+
+            cout<<"not found: "<<targetValue<<endl<<endl;
             return;
         }
 
@@ -77,12 +80,14 @@ class LinkedList
 
         if(head==nullptr)
         {
-            cout<<"emptyy"<<endl;
+
+            cout<<"emptyyy"<<endl;
             return;
         }
 
         if(head->data==targetValue)
         {
+
             insertAtBeginning(value);
             return;
         }
@@ -98,51 +103,53 @@ class LinkedList
 
         if(temp==nullptr)
         {
-
-            cout<<"nnot found : "<<targetValue<<endl<<endl;
-            return;
+            cout<<"not found : "<<targetValue<<endl<<endl;
         }
-
-            Node *newNode=new Node();
-            newNode->data=value;
-            newNode->next=temp;
-            prev->next=newNode;
+        Node *newNode=new Node();
+        newNode->data=value;
+        newNode->next=temp;
+        prev->next=newNode;
 
 
         return;
     }
 
-    void display()
-    {
-        Node *temp=head;
+    void display(){
 
-        while(temp!=nullptr)
+        if(head==nullptr)
         {
 
-            cout<<temp->data<<endl;
+            cout<<"emptyyy";
+            return;
+        }
+        Node *temp=head;
+        while(temp!=nullptr){
+
+            cout<<temp->data<<endl<<endl;
+
             temp=temp->next;
         }
+
         return;
     }
+
 
 };
 
 int main()
 {
+
     LinkedList list;
 
     list.insertAtEnd(10);
     list.insertAtEnd(20);
     list.insertAtEnd(30);
-    list.insertAtEnd(40);
 
     list.insertAtBeginning(5);
 
     list.insertAfter(15,10);
-    list.insertAfter(45,40);
 
-    list.insertBefore(25, 30);
-    list.insertBefore(1,5);
+    list.insertBefore(25,30);
 
     list.display();
 
